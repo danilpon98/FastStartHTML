6e0bbda198e3e9108796a231e3ef9f96bcb23ba3
danilpon98 <43674808+danilpon98@users.noreply.github.com>
2019-01-02 00:25:54 +0700

    update gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,38 @@
-var gulp          = require('gulp'),
+var 	gulp          = require('gulp'),
 		gutil         = require('gulp-util' ),
 		sass          = require('gulp-sass'),
-		browsersync   = require('browser-sync'),
+		browserSync   = require('browser-sync'),
 		concat        = require('gulp-concat'),
 		uglify        = require('gulp-uglify'),
 		cleancss      = require('gulp-clean-css'),
 		rename        = require('gulp-rename'),
 		autoprefixer  = require('gulp-autoprefixer'),
-		notify        = require("gulp-notify"),
+		notify        = require('gulp-notify'),
 		rsync         = require('gulp-rsync');
 
 gulp.task('browser-sync', function() {
-	browsersync({
+	browserSync({
 		server: {
 			baseDir: 'app'
 		},
-		// proxy: "domen.loc",
 		notify: false,
 		// open: false,
-		// tunnel: true,
-		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
 	})
 });
 
-gulp.task('sass', function() {
+gulp.task('styles', function() {
 	return gulp.src('app/sass/**/*.sass')
-	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
 	.pipe(rename({ suffix: '.min', prefix : '' }))
 	.pipe(autoprefixer(['last 15 versions']))
 	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
 	.pipe(gulp.dest('app/css'))
-	.pipe(browsersync.reload( {stream: true} ))
+	.pipe(browserSync.stream())
 });
 
-gulp.task('js', function() {
+gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/js/common.js', // Always at the end
@@ -41,7 +40,12 @@ gulp.task('js', function() {
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Mifify js (opt.)
 	.pipe(gulp.dest('app/js'))
-	.pipe(browsersync.reload({ stream: true }))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('rsync', function() {
@@ -59,11 +63,10 @@ gulp.task('rsync', function() {
 	}))
 });
 
-gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
-	gulp.watch('app/sass/**/*.sass', ['sass']);
-	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
-	// gulp.watch('app/**/*.php', browsersync.reload);
-	gulp.watch('app/*.html', browsersync.reload);
-});
 
-gulp.task('default', ['watch']);
+gulp.task('watch', function() {
+	gulp.watch('app/sass/**/*.sass', gulp.parallel('styles'));
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
+	gulp.watch('app/*.html', gulp.parallel('code'))
+});
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
